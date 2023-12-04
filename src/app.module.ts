36c8ea7dddfe80963f0d1b91fabafb27633067b0
Md Zobayer Hossain Mamun <zobayerhossainmamun@gmail.com>
2023-12-04 20:27:26 +0600

    Configure Typeorm,MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module, Logger } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { getEnv } from './utils';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { ApiModule } from './api/api.module';
@@ -12,6 +13,24 @@ const ENV_TYPE = getEnv();
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [`.env.${ENV_TYPE}`, '.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: String(configService.get('MYSQL_HOST')),
+        port: Number.parseInt(configService.get('MYSQL_PORT') ?? '3306'),
+        username: String(configService.get('MYSQL_USERNAME')),
+        password: String(configService.get('MYSQL_PASSWORD')),
+        database: String(configService.get('MYSQL_DATABASE')),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        logging: configService.get('MYSQL_LOGGING'),
+        timezone: '+06:00',
+        cache: {
+          duration: 60000,
+        }
+      }),
     }),
     ApiModule
   ],
